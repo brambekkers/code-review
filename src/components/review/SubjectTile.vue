08e3dd54deefa40643ebc0dc744acae389c07b7c
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-09-18 16:55:12 +0200

    Update the tiles even if they are completed

--- src/components/review/SubjectTile.vue
+++ src/components/review/SubjectTile.vue
@@ -6,30 +6,18 @@ type Subject = typeof subjects.value[0];
 const props = defineProps<{
   subject: Subject;
 }>();
-
-const isComplete = computed(() => props.subject.score === props.subject.totalQuestions);
-
-const navigate = () => {
-  if (isComplete.value) return;
-  navigateTo(`/review/${props.subject.key}`);
-};
 </script>
 
 <template>
   <div class="group">
     <div
-      class="relative w-full aspect-square bg-cover rounded transition flex items-center"
+      class="relative w-full aspect-square bg-cover rounded transition flex items-center group-hover:-translate-y-1 group-hover:shadow-xl group-hover:scale-105 cursor-pointer"
       :style="`background-image: url(${subject.img});`"
-      :class="{ 'group-hover:-translate-y-1 group-hover:shadow-xl group-hover:scale-105 cursor-pointer': !isComplete }"
-      @click="navigate"
+      @click="navigateTo(`/review/${props.subject.key}`)"
     >
       <div class="absolute z-0 inset-0 bg-gray-700 opacity-60 rounded-md"></div>
 
-      <div v-if="isComplete" class="relative z-10 mx-auto text-center text-white">
-        <h2 class="text-2xl font-bold">Complete</h2>
-      </div>
-
-      <div v-else class="relative z-10 mx-auto text-center text-white">
+      <div class="relative z-10 mx-auto text-center text-white">
         <h2 class="text-2xl font-extralight">{{ subject.title }}</h2>
         <h3 class="text-md font-semibold">{{ subject.score }} / {{ subject.totalQuestions }}</h3>
       </div>
