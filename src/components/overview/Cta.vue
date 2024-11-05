e1f61be1538cf691939d2c9de844d4ffac4352ff
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-11-05 15:19:48 +0100

    Update text

--- src/components/overview/Cta.vue
+++ src/components/overview/Cta.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center">
+  <div class="bg-black px-20 py-20 mt-32 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center">
     <h2 class="text-white text-3xl md:text-6xl">Build better websites.</h2>
-    <p class="text-slate-500 mt-4 text-lg md:text-xl">Start reviewing code and collaborating with the team.</p>
+    <p class="text-slate-500 mt-4 text-lg md:text-xl">Start reviewing code and collaborating with the team</p>
     <div class="flex mt-5">
       <Button label="Get Started" class="!px-6 !bg-white" severity="secondary" @click="navigateTo('/review')" />
     </div>
--- src/components/overview/Logos.vue
+++ src/components/overview/Logos.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mt-24">
-    <h2 class="text-center text-slate-500">Test our technologies</h2>
+  <div class="mt-36">
+    <h2 class="text-center text-xl font-light text-slate-500">Test our technologies</h2>
     <div class="flex gap-8 md:gap-20 items-center justify-center mt-10 flex-wrap">
       <Icon v-tooltip="'Azure'" class="h-8 md:h-12" name="teenyicons:azure-solid" size="48" />
       <Icon v-tooltip="'Vue'" class="h-8 md:h-14" name="simple-icons:vuedotjs" size="48" />
