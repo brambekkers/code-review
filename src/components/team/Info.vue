180c43279f4610ac46f4d443f7734addb9405d55
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2025-05-22 16:34:40 +0200

    feat: enhance form validation and layout in project information page

--- src/components/team/Info.vue
+++ src/components/team/Info.vue
@@ -18,39 +18,39 @@ const removeReviewer = (remover: string) => {
 </script>
 
 <template>
-  <div class="max-w-[600px]">
+  <div class="w-full">
     <p class="text-sm mt-2 mb-5">
       Please fill out the following fields with the relevant information:
     </p>
     <Fieldset
       legend="Project details"
-      class="mx-auto! max-w-4xl! bg-slate-50! w-full"
+      class="mx-auto !bg-slate-50 w-full "
       pt:content="pb-0!"
       pt:legend="text-xl bg-transparent!"
     >
-      <ul>
+      <ul class="grid grid-cols-2 gap-x-6">
           <li
             v-for="(item, key) in team"
-            class="flex py-3 not-last:border-b border-secondary"
+            class="flex py-3 not-last:border-b border-secondary !w-full"
           >
             <!-- Keys -->
-            <div class="w-1/3 capitalize">{{ formatKey(key) }}</div>
+            <div class="w-1/3 capitalize flex items-center">{{ formatKey(key) }}</div>
 
             <!-- Value if Date -->
-            <div v-if="key === 'date'">
+            <div v-if="key === 'date'" class="flex items-center !w-full mr-auto">
               <!-- {{ formatDate(item.date) }} -->
-              <div class="flex flex-col gap-2">
+              <div class="">
                 <DatePicker :id="key" v-model="team[key]" />
               </div>
             </div>
 
             <!-- Value if Array -->
-            <div v-else-if="key === 'reviewers'">
-              <div class="flex w-full gap-2">
+            <div v-else-if="key === 'reviewers'" class="w-full">
+              <div class="flex !w-full gap-2">
                 <InputText
                   type="text"
                   v-model="nameField"
-                  class="grow"
+                  class="grow !w-full"
                   placeholder="Reviewer name"
                 />
                 <Button
@@ -58,6 +58,7 @@ const removeReviewer = (remover: string) => {
                   severity="contrast"
                   @click="addReviewer()"
                   :disabled="!nameField"
+                  class=" whitespace-nowrap px-9!"
                 />
               </div>
               <div v-if="!reviewers.length" class="text-sm mt-2 text-gray-500">
@@ -76,9 +77,10 @@ const removeReviewer = (remover: string) => {
                 </OverlayBadge>
               </div>
             </div>
-            <div v-else>
+            <div class="w-full" v-else>
               <InputText
                 type="text"
+                class="w-full"
                 :id="key"
                 v-model="team[key]"
               />
--- src/pages/project-information/index.vue
+++ src/pages/project-information/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { useVuelidate } from '@vuelidate/core'
-import { required } from '@vuelidate/validators'
+import { useVuelidate } from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
 
 const { team, reviewers } = useTeamStore();
 
@@ -18,10 +18,11 @@ const v$ = useVuelidate(rules, { team });
 const checkFields = () => {
   v$.value.$touch();
   if (!v$.value.$invalid) {
-    navigateTo('/review');
+    navigateTo("/review");
   }
 };
 
+v$.value.$touch()
 </script>
 
 <template>
@@ -30,13 +31,32 @@ const checkFields = () => {
       <template v-slot:title>Project information</template>
       <template v-slot:desc>Filling in your info</template>
     </LayoutSectionHead>
-    <div class="flex flex-col gap-3 mx-auto mt-16">
+    <div class="flex flex-col gap-3 mx-auto mt-16 items-center max-w-4xl!">
       <TeamInfo />
-      <div v-if="v$.team.$error" class="text-red-500 text-sm">
-        Please fill in all required fields.
-      </div>
+
       <div class="flex w-full gap-2">
-        <Button label="Start review" severity="contrast" @click="checkFields()" />
+        <Button
+          :severity="v$.$errors.length > 0 ? 'secondary' : 'success'"
+          label="Start review"
+          @click="checkFields()"
+          class="ml-auto"
+          :class="[v$.$errors.length > 0 ? 'pointer-events-none' : '']"
+          >
+          <Icon name="ic:baseline-rate-review" size="22" />
+          Start review
+        </Button>
+      </div>
+      <div v-if="v$.team.$error" class="text-red-500 text-sm">
+        <ul>
+          <li v-if="v$.team.teamName.$error">Team Name is required.</li>
+          <li v-if="v$.team.applicationName.$error">
+            Application Name is required.
+          </li>
+          <li v-if="v$.team.reviewers.$error">
+            At least one Reviewer is required.
+          </li>
+          <li v-if="v$.team.repoUrl.$error">Repository URL is required.</li>
+        </ul>
       </div>
     </div>
   </div>
