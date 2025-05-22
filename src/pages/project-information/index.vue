499f95d8c56a1a0ae544a71d713ab41905709778
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2025-05-22 15:30:37 +0200

    feat: add Vuelidate for form validation in project information page

--- src/pages/project-information/index.vue
+++ src/pages/project-information/index.vue
@@ -1,10 +1,27 @@
 <script setup>
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
 const { team, reviewers } = useTeamStore();
 
-const checkFields = () => {
-  if(team.teamName && team.applicationName && team.reviewers && team.repoUrl)
-    navigateTo('/review')
+const rules = {
+  team: {
+    teamName: { required },
+    applicationName: { required },
+    reviewers: { required },
+    repoUrl: { required },
+  },
 };
+
+const v$ = useVuelidate(rules, { team });
+
+const checkFields = () => {
+  v$.value.$touch();
+  if (!v$.value.$invalid) {
+    navigateTo('/review');
+  }
+};
+
 </script>
 
 <template>
@@ -15,6 +32,9 @@ const checkFields = () => {
     </LayoutSectionHead>
     <div class="flex flex-col gap-3 mx-auto mt-16">
       <TeamInfo />
+      <div v-if="v$.team.$error" class="text-red-500 text-sm">
+        Please fill in all required fields.
+      </div>
       <div class="flex w-full gap-2">
         <Button label="Start review" severity="contrast" @click="checkFields()" />
       </div>
