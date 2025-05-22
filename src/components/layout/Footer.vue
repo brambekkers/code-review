c098e3c587e835abe2bd967998cf9c0d23b20a5e
Jeffrey-Meesters <jjeffreymeesters@gmail.com>
2025-05-22 11:48:29 +0200

    Hide footer version

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const version = config.public.currentVersion;
+</script>
+
 <template>
   <footer class="my-20">
     <p class="text-center text-sm text-slate-500">Copyright © {{ new Date().getFullYear() }}. All rights reserved.</p>
@@ -5,5 +10,8 @@
       Made by
       <NuxtLink to="https://github.com/brambekkers" target="_blank" rel="noopener" class="hover:underline"> Bram Bekkers & Aliens </NuxtLink>
     </p>
+    <span class="hidden">
+      {{ version }}
+    </span>
   </footer>
 </template>
