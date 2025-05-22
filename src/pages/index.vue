a1389df6e7ced3e25d0aaec7510030286fc6e73b
Jelle Rienstra <jelle.rienstra@nl.abnamro.com>
2025-05-22 12:08:03 +0200

    primevue edit with vue code block

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { ref } from "vue";
+import VCodeBlock from "@wdns/vue-code-block";
+
+const code = ref(`const foo = 'bar';`);
+
+const value = ref("");
+
+const hello = (text) => {
+  value.value = text.htmlValue;
+};
+</script>
+
 <template>
   <div>
     <OverviewHero />
@@ -5,5 +18,20 @@
     <OverviewJustification />
     <OverviewLogos />
     <OverviewCta />
+    <div class="card">
+      <Editor
+        @text-change="hello"
+        v-model="value"
+        editorStyle="height: 320px"
+      />
+      <p>{{ value }}</p>
+      <VCodeBlock
+        :code="value"
+        highlightjs
+        label="Hello World"
+        lang="javascript"
+        theme="neon-bunny"
+      />
+    </div>
   </div>
 </template>
