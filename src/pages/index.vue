c4909d16735e9fa094da4a56ed2c914fac9b8ba7
Jelle Rienstra <jelle.rienstra@nl.abnamro.com>
2025-05-22 15:57:42 +0200

    remove not used package + test code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,3 @@
-<script setup>
-import { ref } from "vue";
-import MdEditor from "~/components/editor/MdEditor.vue";
-import MdPreview from "~/components/editor/MdPreview.vue";
-const text = ref("");
-</script>
-
 <template>
   <div>
     <OverviewHero />
@@ -12,7 +5,5 @@ const text = ref("");
     <OverviewJustification />
     <OverviewLogos />
     <OverviewCta />
-    <MdEditor v-model:text="text" />
-    <MdPreview :text="text" />
   </div>
 </template>
