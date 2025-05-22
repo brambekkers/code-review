f95499f9d50bf7eaee17d9754e8d02f5d2864e3d
Ali Gok <ali.goek@nl.abnamro.com>
2025-05-22 15:51:36 +0200

    fixed the align

--- src/components/score/Topics.vue
+++ src/components/score/Topics.vue
@@ -14,11 +14,10 @@ const hasAnyFeedback = computed(() => subjects.value.some((subject) => subject.t
 const hasNonEmptyComment = (questions: Question[]): boolean => {
   return questions.some(question => question.comment.trim() !== "");
 }
-
 </script>
-
 <template>
-  <Fieldset v-if="hasAnyFeedback" class="max-w-4xl! mx-auto! w-full" legend="Feedback by Subject" pt:content="flex flex-col gap-4" pt:legend="text-xl">
+  <Fieldset v-if="hasAnyFeedback" class="max-w-4xl! mx-auto! w-full" legend="Feedback by Subject"
+    pt:content="flex flex-col gap-4" pt:legend="text-xl">
     <template v-for="subject of subjects" :key="subject.key">
       <section v-if="subject.topics.some((t) => t.comment)" class="">
         <Divider align="center" type="solid" class="mt-4! mb-3!">
@@ -28,17 +27,15 @@ const hasNonEmptyComment = (questions: Question[]): boolean => {
           <template v-if="topic.comment || hasNonEmptyComment(topic.questions)">
             <h4 class="text-sm font-semibold">{{ topic.title }}</h4>
             <div class="bg-slate-50 border rounded-sm p-4">
-              <h5 v-if="topic.comment" class="">
-              </h5>
               <p class="comment text-xl">{{ topic.comment }}</p>
               <div v-for="question of topic.questions" :key="question.question">
-              <div v-if="question.comment">
-              <h6 class="mt-2">
-                <strong>{{ question.question }} </strong>
-              </h6>
-              <p class="comment">{{question.comment}}</p>
+                <div v-if="question.comment">
+                  <h6 class="mt-2">
+                    <strong>{{ question.question }} </strong>
+                  </h6>
+                  <p class="comment">{{ question.comment }}</p>
+                </div>
               </div>
-            </div>
             </div>
           </template>
         </div>
@@ -47,7 +44,7 @@ const hasNonEmptyComment = (questions: Question[]): boolean => {
   </Fieldset>
 </template>
 <style scoped>
-  .comment::first-letter {
-    text-transform: capitalize;
-  }
+.comment::first-letter {
+  text-transform: capitalize;
+}
 </style>
