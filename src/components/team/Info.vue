9e4a8ff89b8aeb691f581bd6bfb7ab1e7e580bc8
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2025-05-22 15:22:32 +0200

    feat: add @vee-validate/yup dependency and update Info.vue component for reviewer management

--- src/components/team/Info.vue
+++ src/components/team/Info.vue
@@ -15,7 +15,6 @@ const removeReviewer = (remover: string) => {
     (reviewer) => reviewer !== remover
   );
 };
-
 </script>
 
 <template>
@@ -30,61 +29,62 @@ const removeReviewer = (remover: string) => {
       pt:legend="text-xl bg-transparent!"
     >
       <ul>
-        <li
-          v-for="(item, key) in team"
-          class="flex py-3 not-last:border-b border-secondary"
-        >
+          <li
+            v-for="(item, key) in team"
+            class="flex py-3 not-last:border-b border-secondary"
+          >
+            <!-- Keys -->
+            <div class="w-1/3 capitalize">{{ formatKey(key) }}</div>
 
-        {{ key }}----
-          <!-- Keys -->
-          <div class="w-1/3 capitalize">{{ formatKey(key) }}</div>
-
-          <!-- Value if Date -->
-          <div v-if="key === 'date'">
-            <!-- {{ formatDate(item.date) }} -->
-            <div class="flex flex-col gap-2">
-              <DatePicker :id="key" v-model="team[key]" />
+            <!-- Value if Date -->
+            <div v-if="key === 'date'">
+              <!-- {{ formatDate(item.date) }} -->
+              <div class="flex flex-col gap-2">
+                <DatePicker :id="key" v-model="team[key]" />
+              </div>
             </div>
-          </div>
 
-          <!-- Value if Array -->
-          <div v-else-if="key === 'reviewers'">
-            <div class="flex w-full gap-2">
+            <!-- Value if Array -->
+            <div v-else-if="key === 'reviewers'">
+              <div class="flex w-full gap-2">
+                <InputText
+                  type="text"
+                  v-model="nameField"
+                  class="grow"
+                  placeholder="Reviewer name"
+                />
+                <Button
+                  label="Add reviewer"
+                  severity="contrast"
+                  @click="addReviewer()"
+                  :disabled="!nameField"
+                />
+              </div>
+              <div v-if="!reviewers.length" class="text-sm mt-2 text-gray-500">
+                Fill in name to add reviewer(s) for this project
+              </div>
+              <div v-else class="flex gap-3 mt-2 pt-2">
+                <OverlayBadge
+                  v-for="reviewer of reviewers"
+                  severity="danger"
+                  value="x"
+                  size="small"
+                  pt:pcbadge:root="cursor-pointer min-w-4! h-4!"
+                  @click="removeReviewer(reviewer)"
+                >
+                  <Chip :label="reviewer" class="text-sm! py-1!" />
+                </OverlayBadge>
+              </div>
+            </div>
+            <div v-else>
               <InputText
                 type="text"
-                v-model="nameField"
-                class="grow"
-                placeholder="Reviewer name"
-              />
-              <Button
-                label="Add reviewer"
-                severity="contrast"
-                @click="addReviewer()"
-                :disabled="!nameField"
+                :id="key"
+                v-model="team[key]"
               />
             </div>
-            <div v-if="!reviewers.length" class="text-sm mt-2 text-gray-500">
-              Fill in name to add reviewer(s) for this project
-            </div>
-            <div v-else class="flex gap-3 mt-2 pt-2">
-              <OverlayBadge
-                v-for="reviewer of reviewers"
-                severity="danger"
-                value="x"
-                size="small"
-                pt:pcbadge:root="cursor-pointer min-w-4! h-4!"
-                @click="removeReviewer(reviewer)"
-              >
-                <Chip :label="reviewer" class="text-sm! py-1!" />
-              </OverlayBadge>
-            </div>
-          </div>
-          <div v-else>
-            <InputText type="text" :id="key" v-model="team[key]" />
-          </div>
-        </li>
+          </li>
       </ul>
-
     </Fieldset>
   </div>
 </template>
